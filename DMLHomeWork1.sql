2314920b1e54ebf8bc392b5132b4ed769df0acb8
suren-vanyan <vanyansuren@gmail.com>
2019-02-05 18:45:16 +0400

    add values

--- DMLHomeWork1.sql
+++ DMLHomeWork1.sql
@@ -7,6 +7,10 @@
 
  --2․Ստեղծել "Freestyle" table, որը պետք է ունենա մինիմում 10 column,
    -- որից միայն 5-6ն է հենց table ստեղծելու ժամանակ տրվում (ցանկալի է տարբեր տիպեր օգտագործել) 
+
+IF OBJECT_ID('MixedMartialArtist') IS NOT NULL
+ DROP TABLE MixedMartialArtist
+
  CREATE TABLE MixedMartialArtist
  (
 	Id INT NOT NULL IDENTITY,
@@ -23,7 +27,7 @@
  ADD [Height] TINYINT DEFAULT 0,
      [Weight] TINYINT DEFAULT 0,
 	 [Division] CHAR(10)	
-
+--------------------------------------
  --4.Փոփոխել column-երից մի քանիսը
 ALTER TABLE MixedMartialArtist
 ALTER COLUMN [HEIGHT] INT 
@@ -36,7 +40,21 @@ ALTER COLUMN [Division] VARCHAR(15)
  
 ALTER TABLE MixedMartialArtist
 ADD PRIMARY KEY(Id)
- 
+
+ALTER TABLE MixedMartialArtist
+ADD CONSTRAINT DF_MixedMartialArtist_Nick DEFAULT 'UNKNOWN' FOR Nickname
+
+ALTER TABLE MixedMartialArtist
+DROP DF_MixedMartialArtist_Nick
+---------------------------------
+ INSERT MixedMartialArtist
+ VALUES
+ (30,'Khabib','Nurmagomedov',DEFAULT,'American',178,70,'Welterweight'),
+ (34,'Tony','Ferguson','El Cucuy','American',180,70,'Welterweight'),
+ (36,'Tyron','Woodley','TheChosenOneTWood','American',175,77,'Welterweight'),
+ (30,'Conor','McGregor','TheNotoriousMystMac','Ireland',175,70,'Welterweight')
+
+
  --CREATE TABLE  MixedMartialArtsRecord 
  IF OBJECT_ID('MixedMartialArtsRecord') IS NOT NULL
  DROP TABLE MixedMartialArtsRecord
